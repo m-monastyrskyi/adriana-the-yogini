1932552e168cc1f8aa5d30bb60e0c505875a07b4
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-07-27 15:38:28 +0200

    parallax position fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,11 +15,11 @@ import img04HQ from "../../assets/img_04_hq.jpg"
 import {Parallax} from "react-parallax";
 
 const Home = ({data}) => {
-    const img1Top = window.innerWidth < 700 ? `8vw` : window.innerWidth < 1900 ? `-5vw` : "-3vw";
-    const img1Height = window.innerWidth < 700 ? `50vh` : window.innerWidth < 1900 ? `80vh` : "80vh";
+    const img1Top = window.innerWidth < 700 ? `32px` : window.innerWidth < 1900 ? `-10px` : "-50px";
+    const img1Height = window.innerWidth < 700 ? `320px` : window.innerWidth < 1900 ? `600px` : "1600px";
     const img1Strength = window.innerWidth < 700 ? 100 : window.innerWidth < 1900 ? 100 : 300;
 
-    const img2Top = window.innerWidth < 700 ? `1vw` : window.innerWidth < 1900 ? `-4vw` : "1vw";
+    const img2Top = window.innerWidth < 700 ? `10px` : window.innerWidth < 1900 ? `-32px` : "20px";
     const img2Height = window.innerWidth < 700 ? `320px` : window.innerWidth < 1900 ? `700px` : "1200px";
     const img2Left = window.innerWidth < 700 ? `100px` : window.innerWidth < 1900 ? `50%` : "50%";
     const img2Strength = window.innerWidth < 700 ? 100 : window.innerWidth < 1900 ? 100 : 500;
@@ -32,7 +32,8 @@ const Home = ({data}) => {
 
             <section>
                 {/*<Parallax bgImage={img03} strength={Math.round(window.innerWidth / 4)} bgImageStyle={{top: window.innerWidth > 700 ? `-6vw` : `8vw`}}>*/}
-                <Parallax bgImage={window.innerWidth < 700 ? img03Mobile : window.innerWidth < 1700 ? img03 : img03HQ} strength={img1Strength} bgImageStyle={{top: img1Top}}>
+                <Parallax bgImage={window.innerWidth < 700 ? img03Mobile : window.innerWidth < 1700 ? img03 : img03HQ}
+                          strength={img1Strength} bgImageStyle={{top: img1Top}}>
                     <div style={{height: img1Height, maxHeight: "1000px"}}>
 
                     </div>
@@ -43,7 +44,8 @@ const Home = ({data}) => {
             <Offer offersList={data.offersList}/>
 
             <section>
-                <Parallax bgImage={window.innerWidth < 700 ? img04Mobile : window.innerWidth < 1700 ? img04 : img04HQ} strength={img2Strength}
+                <Parallax bgImage={window.innerWidth < 700 ? img04Mobile : window.innerWidth < 1700 ? img04 : img04HQ}
+                          strength={img2Strength}
                           bgImageStyle={{top: img2Top, left: img2Left}}
                 >
                     <div style={{height: img2Height}}>
